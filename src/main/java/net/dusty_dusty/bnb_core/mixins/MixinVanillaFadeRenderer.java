4b3fb2b5b46dc2d90c856afde60ccd04ce088a19
ada_aster <an.ada.poirier@gmail.com>
2025-12-15 19:34:19 -0500

    Repair Dh Rendering Objects

--- src/main/java/net/dusty_dusty/bnb_core/mixins/MixinVanillaFadeRenderer.java
+++ src/main/java/net/dusty_dusty/bnb_core/mixins/MixinVanillaFadeRenderer.java
@@ -3,7 +3,7 @@ package net.dusty_dusty.bnb_core.mixins;
 import com.seibel.distanthorizons.core.render.renderer.VanillaFadeRenderer;
 import com.seibel.distanthorizons.core.render.renderer.shaders.FadeApplyShader;
 import com.seibel.distanthorizons.core.render.renderer.shaders.VanillaFadeShader;
-import net.dusty_dusty.dh_dimension_switch.LodRenderChecker;
+import net.dusty_dusty.bnb_core.lod_handling.LodRenderChecker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
