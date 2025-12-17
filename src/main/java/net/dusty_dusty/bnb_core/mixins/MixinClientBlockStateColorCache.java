4c0735a85c00db14644425466f88a2898bb745cd
ada_aster <an.ada.poirier@gmail.com>
2025-12-17 11:46:35 -0500

    Reformat

--- src/main/java/net/dusty_dusty/bnb_core/mixins/MixinClientBlockStateColorCache.java
+++ src/main/java/net/dusty_dusty/bnb_core/mixins/MixinClientBlockStateColorCache.java
@@ -1,18 +1,23 @@
 package net.dusty_dusty.bnb_core.mixins;
 
+import com.seibel.distanthorizons.core.logging.DhLogger;
 import loaderCommon.forge.com.seibel.distanthorizons.common.wrappers.block.ClientBlockStateColorCache;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.material.WaterFluid;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.dusty_dusty.bnb_core.BnbCore;
 
 import java.util.List;
 
@@ -37,6 +42,10 @@ public abstract class MixinClientBlockStateColorCache {
 //        }
 //    }
 
+    @Shadow
+    @Final
+    private static DhLogger LOGGER;
+
     /**
      * @author Ada Aster
      * @reason Injector not working ;-;
@@ -44,15 +53,14 @@ public abstract class MixinClientBlockStateColorCache {
     @Overwrite( remap = false )
     private List<BakedQuad> getQuadsForDirection(@Nullable Direction direction) {
 		List<BakedQuad> quads;
+        BlockState blockState = this.blockState;
 
-        if ( this.blockState.getBlock() instanceof SlabBlock ) {
-            BlockState fullBlockState = this.blockState.setValue( SlabBlock.TYPE, SlabType.DOUBLE );
-            quads = Minecraft.getInstance().getModelManager().getBlockModelShaper().
-                    getBlockModel( fullBlockState ).getQuads( fullBlockState, direction, RANDOM);
-        } else {
-            quads = Minecraft.getInstance().getModelManager().getBlockModelShaper().
-                    getBlockModel(this.blockState).getQuads(this.blockState, direction, RANDOM);
+        if (this.blockState.getBlock() instanceof SlabBlock)
+        {
+            blockState = this.blockState.setValue( SlabBlock.TYPE, SlabType.DOUBLE );
         }
+        quads = Minecraft.getInstance().getModelManager().getBlockModelShaper().
+                getBlockModel( blockState ).getQuads( blockState, direction, RANDOM);
 
 		return quads;
 	}
